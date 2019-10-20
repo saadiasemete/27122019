c36ecd6b5f649dfccabcc04ef4ed1bcea1abaa3d
saadiasemete <38887932+saadiasemete@users.noreply.github.com>
2019-10-20 19:57:16 +0300

    new_post dummy added

---
+++ new_post.py
@@ -0,0 +1,42 @@
+def create_post(board_id, post_data, db_session):
+    """
+    The order of checks matters for user experience.
+    It should be this way:
+    1. Schema check: preventing malformed requests.
+    2. Ban check (all levels): goes BEFORE everything else. People shouldn't waste time.
+    3. Requirements check (all levels).
+    4. Captcha check: only when everything else is fine.
+    """
+
+    #checking for basic validity
+    if not isinstance(post_data, dict): #should be a valid json dict
+        return (False, "Unparseable post_data",)
+    try:
+        assert board_id #should be not null
+        board_id = int(board_id) #should be an integer
+    except:
+        return (False, "Invalid board_id",)
+    
+    #make sure you post to existing board
+    
+    #is thread or post
+    post_data['is_thread'] = not post_data.get('to_thread')
+
+    #make sure you post to existing thread
+    if post_data.get('is_thread'):
+        post_data['to_thread']
+    
+    #thread-scope ban check (if applicable)
+    db_session
+
+    #board-scope ban check
+
+    #global-scope ban check
+
+
+    #TODO: thread requirements check (if applicable)
+
+    #board requirements check
+
+    #TODO: captcha check
+    
