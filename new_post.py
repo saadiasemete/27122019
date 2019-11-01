1f2916317e3d80e6ca09ba6f93547ab858d0752e
saadiasemete <38887932+saadiasemete@users.noreply.github.com>
2019-11-01 20:57:11 +0300

    http statuses

--- new_post.py
+++ new_post.py
@@ -5,12 +5,12 @@ from sqlalchemy import and_
 
 def is_invalid_data(post_data, db_session):
     if not isinstance(post_data, dict): #should be a valid json dict
-        return "Unparseable post_data"
+        return (400, "Unparseable post_data")
     try:
         assert board_id #should be not null
         post_data['id_board'] = int(post_data['id_board']) #should be an integer
     except:
-        return "Invalid board_id"
+        return (404, "Invalid board_id")
     return None
 
 def is_thread(post_data, db_session):
@@ -19,17 +19,17 @@ def is_thread(post_data, db_session):
 def is_board_inexistent(post_data, db_session):
     board_result = db_session.query(Board.id).filter(Board.id == post_data['id_board']).all()
     if not len(board_result):
-        return "board_id does not exist"
+        return (404, "board_id does not exist")
     elif len(board_result)>1:
-        return "Ambiguous board_id"
+        return (500, "Ambiguous board_id")
     return None
 
 def is_thread_inexistent(post_data, db_session):
     post_result = db_session.query(Post.id).filter(Post.id == post_data['to_thread']).all()
     if not len(post_result):
-        return "to_thread does not exist"
+        return (404, "to_thread does not exist")
     elif len(post_result)>1:
-        return "Ambiguous to_thread"
+        return (500, "Ambiguous to_thread")
     return None
 
 def is_banned(post_data, db_session):
@@ -37,15 +37,15 @@ def is_banned(post_data, db_session):
     if db_session.exists(ban_result):
         if post_data['to_thread']:
             if db_session.exists(ban_result.filter(Ban.thread_id == post_data['to_thread'])):
-                return "Banned in the thread"
+                return (403, "Banned in the thread")
         if db_session.exists(ban_result.filter(Ban.board_id == post_data['id_board'])):
-            return "Banned on the board"
+            return (403, "Banned on the board")
     return None
 
 def is_board_rule_violated(post_data, db_session):
     board_result = db_session.query(Board).filter(Board.id == post_data['id_board'] ).first()
     if board_result.read_only:
-        return  "This board is read only"
+        return  (403, "This board is read only")
     return None
 def is_thread_rule_violated(post_data, db_session):
     """
@@ -63,9 +63,9 @@ def is_captcha_failed(post_data, db_session):
             )
         ).first()
         if not captcha_result:
-            return "The captcha is invalid or expired"
+            return (403, "The captcha is invalid or expired")
         elif captcha_result.answer != post_data['captcha_answer']:
-            return "Wrong answer to the captcha"
+            return (403, "Wrong answer to the captcha")
 
 def apply_transformations(post_data, db_session):
     """
@@ -97,7 +97,7 @@ def create_post(post_data, db_session):
     else:
         db_session.query(Post).filter(id == post_data['reply_to']).first().timestamp_last_bump = post_data['timestamp'] 
     db_session.commit
-    return (True, new_post)
+    return (201, new_post)
 
 
 
@@ -140,6 +140,6 @@ def submit_post(post_data, db_session):
         if (i.get('condition') and i['condition'](post_data, db_session)) or not i.get('condition'):
             err_status = i['checker'](post_data, db_session)
             if err_status:
-                return (False, err_status)
+                return err_status
     post_data['timestamp'] = int(time.time())
     return create_post(post_data, db_session)
